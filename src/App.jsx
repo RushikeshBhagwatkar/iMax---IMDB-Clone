ab88c587de42ad2d0e868e85fd2473e135e9f8d4
RushikeshBhagwatkar <92802693+RushikeshBhagwatkar@users.noreply.github.com>
2024-07-28 16:09:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,62 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import {BrowserRouter, Routes, Route}  from 'react-router-dom'  //to create router
-import Movies from './components/Movies'
-import Watchlist from './components/Watchlist'
-import Banner from './components/Banner'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; //to create router
+import Movies from "./components/Movies";
+import Watchlist from "./components/Watchlist";
+import Banner from "./components/Banner";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [watchlist, setWatchList] = useState([]);
+
+  const handleAddtoWatchList = (movieObj) => {
+    let newWatchList = [...watchlist, movieObj]; //it will take older watchlist and add new movie
+    localStorage.setItem('moviesApp', JSON.stringify(newWatchList))
+    setWatchList(newWatchList);
+    console.log(newWatchList);
+  };
+
+  let handleRemoveFromWatchList = (movieObj) => {
+    let filteredWatchList = watchlist.filter((movie) => {
+      return movie.id != movieObj.id;
+    });
+    setWatchList(filteredWatchList);
+    console.log(filteredWatchList);
+  };
+
+  useEffect(() => {
+    let moviesFromLocalStorage = localStorage.getItem('moviesApp')
+    if(!moviesFromLocalStorage){
+      return
+    }
+    setWatchList(JSON.parse(moviesFromLocalStorage))
+  }, [])
+  
 
   return (
     <>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<><Banner/><Movies/></>} />
-          <Route path='/watchlist' element={<Watchlist/>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner />
+                <Movies
+                  watchlist={watchlist}
+                  handleAddtoWatchList={handleAddtoWatchList}
+                  handleRemoveFromWatchList={handleRemoveFromWatchList}
+                />
+              </>
+            }
+          />
+          <Route path="/watchlist" element={<Watchlist watchlist={watchlist} setWatchList={setWatchList}/>} />
         </Routes>
       </BrowserRouter>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
