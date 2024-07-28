fca5ab886ceccd349aec2a9b7746d13aeffb9044
RushikeshBhagwatkar <92802693+RushikeshBhagwatkar@users.noreply.github.com>
2024-07-28 16:12:53 +0530

    Update Pagination.jsx

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Pagination = () => {
+const Pagination = ({handlePrev , handleNex, pageNo}) => {
   return (
-    <div className='bg-gray-400 p-4 mt-8 flex justify-center'>Pagination</div>
+    <div className='bg-gray-400 text-black p-4 mt-8 w-full flex justify-center'>
+        <div onClick={handlePrev} className='px-8 hover:cursor-pointer'><i class="fa-solid fa-arrow-left"></i></div>
+        <div className='font-bold'>{pageNo}</div>
+        <div onClick={handleNex} className='px-8 hover:cursor-pointer'>
+        <i className="fa-solid fa-arrow-right"></i>
+        </div>
+    </div>
   )
 }
 
